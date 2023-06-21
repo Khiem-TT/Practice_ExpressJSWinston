832a4fb29593d112a4cd12c79ca876150de59295
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-06-21 11:05:45 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {logger} from "./src/winston";
+
+const port = 8000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    try {
+        res.end('<h1>Hello winston!</h1>');
+        throw new Error('Error test winston');
+    } catch (err) {
+        logger.error(err);
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
